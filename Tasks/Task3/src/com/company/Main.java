973ca7baf74cf6cdd7d5a75982bac7a2c6e3ea91
Student20 <Student20@itpark.ru>
2017-09-25 21:01:08 +0300

    add classworks

---
+++ Tasks/Task3/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    scanner scanner
+        int code = 1041;
+        char character = (char)
+	    System.out.println();
+    }
+    }
+
---
+++ Tasks/Task4/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MainArraysWithFor {
+
+    public static void main(String[] args) {
+	    int array[] = {3, 5, 2 , 3, 1, 6, 9};
+        for (int i = 0; i < array.length; i++) {
+        int temp = array[i];
+        array [i] = array[array.length - 1 - i]
+            array (n-l-j) = temp;
+        for(int 1 = 0; i < array.lenght; i++)
+            System.out.print(array[i] + " ");
+                }
+    }
+}
